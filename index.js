8254eac200f4f03204df7fd91c1deeb8f9cdd85d
FranCaprari <francaprari25@gmail.com>
2022-05-12 19:59:50 -0300

    Agregando eventos y modificando dom

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
-const productos = [
-    {nombre: "Proteina", precio: 4500},
-    {nombre: "Creatina", precio: 3500},
-    {nombre: "Vitaminas", precio: 2500},
+const contenedor = document.getElementById("productos");
+const Productos = [
+    { id: 1,
+      nombre: "Creatina 150g",  
+      precio: 3500,
+      imagen: "images/creatina150g.png",
+    },
+    { id: 2,
+        nombre: "Whey protein NITRO",  
+        precio: 5500,
+        imagen: "images/wheynitrov.png",
+      },
+      { id: 3,
+        nombre: "Whey protein",  
+        precio: 4500,
+        imagen: "images/whey2lbv.png",
+      },
+      { id: 4,
+        nombre: "Multivitaminico",  
+        precio: 2500,
+        imagen: "images/vitaminas.png",
+      },
+      { id: 5,
+        nombre: "Whey protein Avena",  
+        precio: 4500,
+        imagen: "images/wheyavena.png",
+      },
+      { id: 6,
+        nombre: "Whey protein WOMAN",  
+        precio: 4500,
+        imagen: "images/wheywoman.png",
+      },
+
 ];
 
-for(const producto of productos){
-    let contenedor = document.createElement("div");
-    contenedor.innerHTML = `<img src="images/creatina.png"></img>
-                            <h2>PRODUCTO: ${producto.nombre}</h2>
-                            <p>PRECIO: ${producto.precio}</p>
-                            <button>Agregar al carrito</button>`;
-    document.body.append(contenedor);
+
+const construirCarta = (item) => {
+    return(
+        `<div class="card" style="width: 15rem;">
+        <img src="${item.imagen}" alt="${item.nombre}" class="card-img-top ">
+        <div class="card-body">
+          <h3 class="card-text">${item.nombre}</h3>
+          <p class="card-text">Descripcion.....</p>
+          <p class="card-text">Precio: $${item.precio}</p>
+          <a href="#" onclick=avisoCarrito() class="btn btn-primary productos__precio">Agregar al carrito</a>
+        </div>
+      </div>`
+    );
 }
 
+const cargarProductos = (info, nodo) =>{
+    let acumulador = "";
+    info.forEach((elem) =>{
+        acumulador += construirCarta(elem);
+    })
+    nodo.innerHTML = acumulador; 
+}
+
+const avisoCarrito = ()=>{
+    alert("Agregaste el producto al carrito");
+}
+cargarProductos(Productos, contenedor);
